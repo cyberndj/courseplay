6008d19e6ae271f405cf856eda3f356e06f5f18b
Peter Vaiko <github@vaiko.us>
2020-07-15 07:00:19 -0500

    wip

--- AIDriver.lua
+++ AIDriver.lua
@@ -1655,7 +1655,7 @@ end
 
 function AIDriver:updateTrafficConflictDetector()
 	if self.trafficConflictDetector and self.course and g_updateLoopIndex % 2 == 0 then 
-		self.trafficConflictDetector:update(self.course, self.course:getCurrentWaypointIx()) 
+		self.trafficConflictDetector:update(self.course, self.ppc:getRelevantWaypointIx())
 	end
 end
 
